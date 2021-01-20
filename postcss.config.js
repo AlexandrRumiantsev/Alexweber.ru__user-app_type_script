ac3e1a66944a35b3f2010b9981d724e1405e57c5
alex <tte577es7@yandex.ru>
2021-01-20 14:44:07 +0300

    Новый шаблон

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,5 @@
-module.exports = ({ file, options, env }) => ({
-  parser: false,
-  plugins: {
-    'postcss-import': {},
-    'postcss-cssnext': {},
-    cssnano: env === 'production' ? {} : false,
-  },
-});
+module.exports = {
+  plugins: [
+    require('postcss-preset-env')()
+  ]
+}
